0300496c33158536af899867c32e5c1e3822a524
whitehatchamp111 <83204573+whitehatchamp111@users.noreply.github.com>
2021-08-02 12:49:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var movingRect, fixedRect;
+
+function setup() {
+  createCanvas(800,600);
+  
+  fixedRect=createSprite(400,200,50,80);
+  fixedRect.shapeColor="green";
+
+  movingRect=createSprite(400,400,80,50);
+  movingRect.shapeColor="green";
+
+  movingRect.debug=true;
+  fixedRect.debug=true;
+ 
+ 
+  
+}
+function draw() {
+  background(0);
+  
+  movingRect.x=World.mouseX;
+  movingRect.y=World.mouseY;
+  
+  if((movingRect.x- fixedRect.x < fixedRect.width/2+movingRect.width/2)
+  &&(fixedRect.x -movingRect.x< fixedRect.width/2+movingRect.width/2)
+  &&(movingRect.x- fixedRect.x < fixedRect.height/2+movingRect.height/2)
+  &&(fixedRect.x -movingRect.x< fixedRect.height/2+movingRect.height/2)){
+
+               movingRect.shapeColor="red";
+               fixedRect.shapeColor="red";
+  }
+  else{
+     movingRect.shapeColor="green";
+     fixedRect.shapeColor="green";
+  }
+
+  
+  drawSprites();
+}
